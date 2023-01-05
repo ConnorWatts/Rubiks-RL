29276dd343a50be11d5eecae9012f93c99cd748b
Connor Watts <connorwatts@hotmail.com>
2023-01-05 15:05:01 +0000

    Cube Class

---
+++ cube.py
@@ -0,0 +1,18 @@
+
+
+class Cube():
+    def __init__(self) -> None:
+        pass
+
+    def get_sides(self) -> int:
+        pass
+
+    def is_solved(self) -> bool:
+        pass
+
+    def scramble(self) -> None:
+        pass
+
+    def draw(self) -> None:
+        pass
+
