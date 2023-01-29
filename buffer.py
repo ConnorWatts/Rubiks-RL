bd1ce812a4ff7f1202a24a3023694c43371736e2
Connor Watts <connorwatts@hotmail.com>
2023-01-29 17:02:03 +0000

    Buffer to do

--- buffer.py
+++ buffer.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import random
 import numpy as np
+import torch
 
 from typing import Dict, List, Tuple, Union
 
@@ -7,16 +9,29 @@ class ReplayBuffer:
     def __init__(self,config) -> None:
         super().__init__()
 
-        self.buffer = pd.DataFrame(columns=['state', 'next state','action', 'reward','done'])
+        # individual buffers
+        # maybe store as array of zeros - might be better on memory
+        self.state_buffer = np.array([])
+        self.next_state_buffer =np.array([])
+        self.action_buffer = np.array([])
+        self.reward_buffer = np.array([])
+        self.done_buffer = np.array([])
 
-    def sample(self, batch_size: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray] :
-        sample = self.buffer.sample(batch_size)
-        return sample['state'].to_numpy(), sample['next state'].to_numpy(), sample['action'], sample['reward'], sample['done']
+    def sample(self, batch_size: int) -> Tuple[torch.Tensor, torch.Tensor, np.ndarray, np.ndarray, np.ndarray] :
+        rand_idx = random.sample(range(0, len(self.state_buffer)), batch_size)
+        states = torch.tensor(self.state_buffer[rand_idx])
+        next_states = torch.tensor(self.next_state_buffer[rand_idx])
+        action = self.action_buffer[rand_idx]
+        reward = self.reward_buffer[rand_idx]
+        done = self.done_buffer[rand_idx]
+        return states, next_states, action, done
 
     def store_transition(self, state, next_state, action, reward, done) -> None:
-        d = {'state':state.tolist(), 'next state':next_state.tolist(),'action':action, 'reward':reward,'done':done}
-        store_df = pd.DataFrame(data= d)
-        self.buffer.append(store_df, ignore_index=True)
+        self.state_buffer.append(state)
+        self.next_state_buffer.append(next_state)
+        self.action_buffer.append(action)
+        self.reward_buffer.append(reward)
+        self.done_buffer.append(done)
 
     def prune(self) -> None:
         # delete early entries of the buffer
